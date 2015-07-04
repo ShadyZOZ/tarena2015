f89e4f1ccc8b20e7768b96e6ac7f5778d4137715
ShadyZOZ <shadyzoz@163.com>
2015-07-04 23:25:17 +0800

    refined & lower iOS SKD to 8.2

--- TaxCalculator/TaxCalculator/ViewController.swift
+++ TaxCalculator/TaxCalculator/ViewController.swift
@@ -20,13 +20,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
     var resultTextField: UITextField!
     var afterTaxIncomeTextField: UITextField!
     
+    var textFields = [UITextField]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // get screen width & height
-        var width = CGRectGetWidth(UIScreen.mainScreen().bounds)
-        var height = CGRectGetHeight(UIScreen.mainScreen().bounds)
+        var width = CGRectGetWidth(self.view.bounds)
+        var height = CGRectGetHeight(self.view.bounds)
         
         // show title
         var title = UILabel(frame: CGRectMake(0, 64, width, 40))
@@ -36,119 +38,101 @@ class ViewController: UIViewController, UITextFieldDelegate {
         title.textAlignment = NSTextAlignment.Center
         self.view.addSubview(title)
         
-        // 视图-- UIView
-        
-        // 收入
+        // 视图 -- UIView
         var incomeView = UIView(frame: CGRectMake(30, 120, width-60, 110))
-        incomeView.backgroundColor = UIColor.orangeColor()
-        self.view.addSubview(incomeView)
-        
-        var salaryLable = UILabel(frame: CGRectMake(25, 5, 100, 30))
-        salaryLable.text = "基本工资:"
-        incomeView.addSubview(salaryLable)
-        
-        salaryTextField = UITextField(frame: CGRectMake(135, 5, 150, 30))
-        salaryTextField.borderStyle = UITextBorderStyle.RoundedRect
-        salaryTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        salaryTextField.keyboardType = UIKeyboardType.DecimalPad
-        salaryTextField.delegate = self
-        incomeView.addSubview(salaryTextField)
-        
-        var bonusLable = UILabel(frame: CGRectMake(25, 40, 100, 30))
-        bonusLable.text = "绩效奖金:"
-        incomeView.addSubview(bonusLable)
-        
-        bonusTextField = UITextField(frame: CGRectMake(135, 40, 150, 30))
-        bonusTextField.borderStyle = UITextBorderStyle.RoundedRect
-        bonusTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        bonusTextField.keyboardType = UIKeyboardType.DecimalPad
-        bonusTextField.delegate = self
-        incomeView.addSubview(bonusTextField)
-        
-        var mealLable = UILabel(frame: CGRectMake(25, 75, 100, 30))
-        mealLable.text = "饭       补:"
-        incomeView.addSubview(mealLable)
-        
-        mealTextField = UITextField(frame: CGRectMake(135, 75, 150, 30))
-        mealTextField.borderStyle = UITextBorderStyle.RoundedRect
-        mealTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        mealTextField.keyboardType = UIKeyboardType.DecimalPad
-        mealTextField.delegate = self
-        incomeView.addSubview(mealTextField)
-        
-        // 保险
         var insuranceView = UIView(frame: CGRectMake(30, 260, width-60, 145))
+        var resultView = UIView(frame: CGRectMake(30, 430, width-60, 75))
+        
+        incomeView.backgroundColor = UIColor.orangeColor()
         insuranceView.backgroundColor = UIColor.cyanColor()
+        resultView.backgroundColor = UIColor.lightGrayColor()
+        
+        self.view.addSubview(incomeView)
         self.view.addSubview(insuranceView)
+        self.view.addSubview(resultView)
+
+        // 标签 -- UILabel
+        var salaryLable = UILabel(frame: CGRectMake(25, 5, 100, 30))
+        var bonusLable = UILabel(frame: CGRectMake(25, 40, 100, 30))
+        var mealLable = UILabel(frame: CGRectMake(25, 75, 100, 30))
         
         var retirementInsuranceLable = UILabel(frame: CGRectMake(25, 5, 100, 30))
-        retirementInsuranceLable.text = "养老保险:"
-        insuranceView.addSubview(retirementInsuranceLable)
-        
-        retirementInsuranceTextField = UITextField(frame: CGRectMake(135, 5, 150, 30))
-        retirementInsuranceTextField.borderStyle = UITextBorderStyle.RoundedRect
-        retirementInsuranceTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        retirementInsuranceTextField.keyboardType = UIKeyboardType.DecimalPad
-        retirementInsuranceTextField.delegate = self
-        insuranceView.addSubview(retirementInsuranceTextField)
-        
         var jobInsuranceLable = UILabel(frame: CGRectMake(25, 40, 100, 30))
-        jobInsuranceLable.text = "失业保险:"
-        insuranceView.addSubview(jobInsuranceLable)
-        
-        jobInsuranceTextField = UITextField(frame: CGRectMake(135, 40, 150, 30))
-        jobInsuranceTextField.borderStyle = UITextBorderStyle.RoundedRect
-        jobInsuranceTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        jobInsuranceTextField.keyboardType = UIKeyboardType.DecimalPad
-        jobInsuranceTextField.delegate = self
-        insuranceView.addSubview(jobInsuranceTextField)
-        
         var medicalInsuranceLable = UILabel(frame: CGRectMake(25, 75, 100, 30))
-        medicalInsuranceLable.text = "医疗保险:"
-        insuranceView.addSubview(medicalInsuranceLable)
-        
-        medicalInsuranceTextField = UITextField(frame: CGRectMake(135, 75, 150, 30))
-        medicalInsuranceTextField.borderStyle = UITextBorderStyle.RoundedRect
-        medicalInsuranceTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        medicalInsuranceTextField.keyboardType = UIKeyboardType.DecimalPad
-        medicalInsuranceTextField.delegate = self
-        insuranceView.addSubview(medicalInsuranceTextField)
-        
         var publicFundLable = UILabel(frame: CGRectMake(25, 110, 100, 30))
-        publicFundLable.text = "公  积  金:"
-        insuranceView.addSubview(publicFundLable)
-        
-        publicFundTextField = UITextField(frame: CGRectMake(135, 110, 150, 30))
-        publicFundTextField.borderStyle = UITextBorderStyle.RoundedRect
-        publicFundTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        publicFundTextField.keyboardType = UIKeyboardType.DecimalPad
-        publicFundTextField.delegate = self
-        insuranceView.addSubview(publicFundTextField)
-        
-        // 税费结果
-        var resultView = UIView(frame: CGRectMake(30, 430, width-60, 75))
-        resultView.backgroundColor = UIColor.lightGrayColor()
-        self.view.addSubview(resultView)
         
         var resultLable = UILabel(frame: CGRectMake(25, 5, 100, 30))
-        resultLable.text = "应纳个税:"
-        resultView.addSubview(resultLable)
-        
-        resultTextField = UITextField(frame: CGRectMake(135, 5, 150, 30))
-        resultTextField.borderStyle = UITextBorderStyle.RoundedRect
-        resultTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        resultTextField.enabled = false
-        resultView.addSubview(resultTextField)
-        
         var afterTaxIncomeLabel = UILabel(frame: CGRectMake(25, 40, 100, 30))
+        
+        // 标签内容
+        salaryLable.text = "基本工资:"
+        bonusLable.text = "绩效奖金:"
+        mealLable.text = "饭       补:"
+        
+        retirementInsuranceLable.text = "养老保险:"
+        jobInsuranceLable.text = "失业保险:"
+        medicalInsuranceLable.text = "医疗保险:"
+        publicFundLable.text = "公  积  金:"
+        
+        resultLable.text = "应纳个税:"
         afterTaxIncomeLabel.text = "实际收入:"
+        
+        // 添加视图
+        incomeView.addSubview(salaryLable)
+        incomeView.addSubview(bonusLable)
+        incomeView.addSubview(mealLable)
+        
+        insuranceView.addSubview(retirementInsuranceLable)
+        insuranceView.addSubview(jobInsuranceLable)
+        insuranceView.addSubview(medicalInsuranceLable)
+        insuranceView.addSubview(publicFundLable)
+        
+        resultView.addSubview(resultLable)
         resultView.addSubview(afterTaxIncomeLabel)
         
+        //文本框 -- UITextField
+        salaryTextField = UITextField(frame: CGRectMake(135, 5, 150, 30))
+        bonusTextField = UITextField(frame: CGRectMake(135, 40, 150, 30))
+        mealTextField = UITextField(frame: CGRectMake(135, 75, 150, 30))
+        
+        retirementInsuranceTextField = UITextField(frame: CGRectMake(135, 5, 150, 30))
+        jobInsuranceTextField = UITextField(frame: CGRectMake(135, 40, 150, 30))
+        medicalInsuranceTextField = UITextField(frame: CGRectMake(135, 75, 150, 30))
+        publicFundTextField = UITextField(frame: CGRectMake(135, 110, 150, 30))
+        
+        resultTextField = UITextField(frame: CGRectMake(135, 5, 150, 30))
         afterTaxIncomeTextField = UITextField(frame: CGRectMake(135, 40, 150, 30))
-        afterTaxIncomeTextField.borderStyle = UITextBorderStyle.RoundedRect
-        afterTaxIncomeTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
-        afterTaxIncomeTextField.enabled = false
+
+        // 添加子视图
+        incomeView.addSubview(salaryTextField)
+        incomeView.addSubview(bonusTextField)
+        incomeView.addSubview(mealTextField)
+        
+        insuranceView.addSubview(retirementInsuranceTextField)
+        insuranceView.addSubview(jobInsuranceTextField)
+        insuranceView.addSubview(medicalInsuranceTextField)
+        insuranceView.addSubview(publicFundTextField)
+        
+        resultView.addSubview(resultTextField)
         resultView.addSubview(afterTaxIncomeTextField)
+        
+        textFields = [salaryTextField, bonusTextField, mealTextField, retirementInsuranceTextField, jobInsuranceTextField, medicalInsuranceTextField, publicFundTextField, resultTextField, afterTaxIncomeTextField]
+        
+        // common attributes
+        for textField in textFields
+        {
+            if textField == resultTextField || textField == afterTaxIncomeTextField
+            {
+                textField.enabled = false
+            }
+            else
+            {
+                textField.clearButtonMode = UITextFieldViewMode.WhileEditing
+                textField.keyboardType = UIKeyboardType.DecimalPad
+                textField.delegate = self
+            }
+            textField.borderStyle = UITextBorderStyle.RoundedRect
+        }
         
         // signatural
         var sign = UILabel(frame: CGRectMake(0, height - 70, width, 70))
@@ -166,38 +150,40 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        salaryTextField.resignFirstResponder()
-        bonusTextField.resignFirstResponder()
-        mealTextField.resignFirstResponder()
-        retirementInsuranceTextField.resignFirstResponder()
-        jobInsuranceTextField.resignFirstResponder()
-        medicalInsuranceTextField.resignFirstResponder()
-        publicFundTextField.resignFirstResponder()
+        
+        for view in self.view.subviews
+        {
+            for subView in view.subviews
+            {
+                if subView.isKindOfClass(UITextField)
+                {
+                   subView.resignFirstResponder()
+                }
+            }
+        }
     }
     
-    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+    func textFieldDidBeginEditing(textField: UITextField) {
         textField.text = ""
-        return true
     }
     
     func textFieldShouldEndEditing(textField: UITextField) -> Bool
     {
         if textField == salaryTextField
         {
-            if bonusTextField.text.isEmpty
-            {
-                bonusTextField.text = "0.00"
-            }
-            if mealTextField.text.isEmpty
-            {
-                mealTextField.text = "0.00"
-            }
-            retirementInsuranceTextField.text = Double2String(String2Double(salaryTextField.text) * 0.08)
-            jobInsuranceTextField.text = Double2String(String2Double(salaryTextField.text) * 0.002)
-            medicalInsuranceTextField.text = Double2String(String2Double(salaryTextField.text) * 0.02 + 3)
-            publicFundTextField.text = Double2String(String2Double(salaryTextField.text) * 0.12)
+            retirementInsuranceTextField.text = Float2String(String2Float(salaryTextField.text) * 0.08)
+            jobInsuranceTextField.text = Float2String(String2Float(salaryTextField.text) * 0.002)
+            medicalInsuranceTextField.text = Float2String(String2Float(salaryTextField.text) * 0.02 + 3)
+            publicFundTextField.text = Float2String(String2Float(salaryTextField.text) * 0.12)
         }
-        textField.text = Double2String(String2Double(textField.text))
+        for field in textFields
+        {
+            if field.text.isEmpty
+            {
+                field.text = "0.00"
+            }
+        }
+        textField.text = Float2String(String2Float(textField.text))
         calTax()
         return true
     }
@@ -217,56 +203,56 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    func String2Double(string: String) -> Double
+    func String2Float(string: String) -> Float
     {
-        return (string as NSString).doubleValue
+        return NSString(string: string).floatValue
     }
     
-    func Double2String(num: Double) -> String
+    func Float2String(num: Float) -> String
     {
         return NSString(format: "%.2f", num) as String
     }
     
     func calTax()
     {
-        var totalIncome = String2Double(salaryTextField.text) + String2Double(bonusTextField.text) + String2Double(mealTextField.text)
-        var expance  = String2Double(retirementInsuranceTextField.text) + String2Double(jobInsuranceTextField.text) + String2Double(medicalInsuranceTextField.text) + String2Double(publicFundTextField.text)
+        var totalIncome = String2Float(salaryTextField.text) + String2Float(bonusTextField.text) + String2Float(mealTextField.text)
+        var expance  = String2Float(retirementInsuranceTextField.text) + String2Float(jobInsuranceTextField.text) + String2Float(medicalInsuranceTextField.text) + String2Float(publicFundTextField.text)
         var income = totalIncome - expance
         var taxLevel = income - 3500
         
         if taxLevel <= 0
         {
-            resultTextField.text = Double2String(0.0)
+            resultTextField.text = Float2String(0.0)
         }
         else if taxLevel <= 1500
         {
-            resultTextField.text = Double2String(taxLevel * 0.03)
+            resultTextField.text = Float2String(taxLevel * 0.03)
         }
         else if taxLevel <= 4500
         {
-            resultTextField.text = Double2String(taxLevel * 0.1 - 105)
+            resultTextField.text = Float2String(taxLevel * 0.1 - 105)
         }
         else if taxLevel <= 9000
         {
-            resultTextField.text = Double2String(taxLevel * 0.2 - 555)
+            resultTextField.text = Float2String(taxLevel * 0.2 - 555)
         }
         else if taxLevel <= 35000
         {
-            resultTextField.text = Double2String(taxLevel * 0.25 - 1005)
+            resultTextField.text = Float2String(taxLevel * 0.25 - 1005)
         }
         else if taxLevel <= 55000
         {
-            resultTextField.text = Double2String(taxLevel * 0.3 - 2755)
+            resultTextField.text = Float2String(taxLevel * 0.3 - 2755)
         }
         else if taxLevel <= 80000
         {
-            resultTextField.text = Double2String(taxLevel * 0.35 - 5505)
+            resultTextField.text = Float2String(taxLevel * 0.35 - 5505)
         }
         else
         {
-            resultTextField.text = Double2String(taxLevel * 0.45 - 13505)
+            resultTextField.text = Float2String(taxLevel * 0.45 - 13505)
         }
         
-        afterTaxIncomeTextField.text = Double2String(income - String2Double(resultTextField.text))
+        afterTaxIncomeTextField.text = Float2String(income - String2Float(resultTextField.text))
     }
 }
